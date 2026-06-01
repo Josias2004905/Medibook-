52ebd719f84abf5a82bcca71a6ff4e0066fd4ffc
DJAGBARE Yendougnoime Josias <djagbare.yendougnoimejosias@student.emi.ac.ma>
2026-06-01 19:51:47 +0100

    Remove unused 'Field' import in forms.py
    Removed 'Field' import from crispy_forms.layout.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Row, Column, Submit, Field
+from crispy_forms.layout import Layout, Row, Column, Submit
 from .models import User
 
 
